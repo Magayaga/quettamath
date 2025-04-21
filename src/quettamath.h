044339dc7f3073e61483329813e485e5a69429c7
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2025-04-21 12:31:27 +0800

    Quettamath's source code: Update `quettamath.h`

--- src/quettamath.h
+++ src/quettamath.h
@@ -8,25 +8,23 @@
 
 #include <stddef.h>
 
-// Unary operations
 double qm_positive(double x);
 double qm_negative(double x);
-
-// Variadic arithmetic:
 double qm_add(size_t count, ...);
 double qm_subtract(size_t count, ...);
 double qm_multiply(size_t count, ...);
 double qm_divide(size_t count, ...);
-
-// Variadic extremes
 double qm_min(size_t count, ...);
 double qm_max(size_t count, ...);
-
 double qm_sq(double x);
 double qm_sqrt(double x);
 double qm_fabs(double x);
 double qm_floor(double x);
 double qm_ceil(double x);
 double qm_trunc(double x);
+long long qm_gcd(size_t count, ...);
+long long qm_lcm(size_t count, ...);
+double qm_degrees(double radians);
+double qm_radians(double degrees);
 
 #endif // QUETTAMATH_H
